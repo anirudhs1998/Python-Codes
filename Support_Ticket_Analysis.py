0202e0ca5ac4de5af9be6085ce3e91bec758f597
Anirudh S <anirudh@gmail.com>
2018-05-25 10:57:47 +0530

    Second Commit
    Added prediction accuracy for all 4 column predictions.

--- Support_Ticket_Analysis.py
+++ Support_Ticket_Analysis.py
@@ -13,8 +13,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.metrics import confusion_matrix
 from nltk.stem.snowball import SnowballStemmer
 
-column_to_predict = "urgency"
-
+column_to_predict = pd.Series(["ticket_type","category","urgency","impact"])
 
 class StemmedCountVectorizer(CountVectorizer):
     def build_analyzer(self):
@@ -26,39 +25,40 @@ class StemmedCountVectorizer(CountVectorizer):
 stop_words_lang = 'english'
 fit_prior = True
 min_data_per_class = 1 
-
+y_pred = pd.DataFrame(columns = column_to_predict)
+prediction_acc = pd.DataFrame()
+pred = pd.DataFrame()
 #dataframe = pd.read_csv('all_tickets.csv',dtype=str)
 dataframe = pd.read_csv('all_tickets.csv')
 text_columns = "body"
 #bytag = dataframe.groupby(column_to_predict).aggregate(np.count_nonzero)
 #tags = bytag[bytag.body >= min_data_per_class].index
 #dataframe = dataframe[dataframe[column_to_predict].isin(tags)]
-
-y = dataframe[column_to_predict]
-X = dataframe[text_columns]
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20)
-
-#Removing stop words and performing stemming using PIPELINE
-
-count_vect = CountVectorizer(stop_words=stop_words_lang)
-#count_vect = StemmedCountVectorizer(stop_words=stop_words_lang)
+for i in range(0,4):
+    y = dataframe[column_to_predict[i]]
+    X = dataframe[text_columns]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20)
+    #Removing stop words and performing stemming using PIPELINE
+    count_vect = CountVectorizer(stop_words=stop_words_lang)
+    #count_vect = StemmedCountVectorizer(stop_words=stop_words_lang)
+    #Using Naive Bayes Classifier
+    text_clf = Pipeline([('vect', count_vect),('tfidf', TfidfTransformer()),('clf', MultinomialNB(fit_prior=fit_prior))])
+    #Using SVM Classifier
+    #text_clf = Pipeline([('vect', count_vect),('tfidf', TfidfTransformer()),('clf', SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3,n_iter=5, random_state=42))])
+    text_clf = text_clf.fit(X_train, y_train)
+    y_pred[column_to_predict[i]] = text_clf.predict(X_test)
+    y_test = y_test.reset_index(drop=True)
+    pred = pd.DataFrame(data = [np.mean(y_pred[column_to_predict[i]] == y_test)])
+    prediction_acc = pd.concat([prediction_acc,pred],axis=1)
     
-#Using Naive Bayes Classifier
-text_clf = Pipeline([('vect', count_vect),('tfidf', TfidfTransformer()),('clf', MultinomialNB(fit_prior=fit_prior))])
-#Using SVM Classifier
-text_clf = Pipeline([('vect', count_vect),('tfidf', TfidfTransformer()),('clf', SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3,n_iter=5, random_state=42))])
+prediction_acc.columns = column_to_predict
 
-text_clf = text_clf.fit(X_train, y_train)
-
-'''
-#ticket = "Hi, my outlook app seems to misbehave a lot lately. I cannot sync my emails and it often crashes and asks for credentials.Could you help me out"
-ticket = input("Enter ticket")
+ticket = "Hi, my outlook app seems to misbehave a lot lately. I cannot sync my emails and it often crashes and asks for credentials.Could you help me out"
+#ticket = input("Enter ticket")
 ticket_test = pd.Series(ticket)
 ticket_analysis = text_clf.predict(ticket_test)
-'''
 
-y_pred = text_clf.predict(X_test)
+
 
 cm = confusion_matrix(y_test, y_pred)
 
