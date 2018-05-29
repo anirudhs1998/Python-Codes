4d4101f4587cffc0c344a3d8c8325ae47497c01e
Anirudh S <anirudh@gmail.com>
2018-05-29 16:24:23 +0530

    Fourth Commit
    Used GridSearch to optimise parameters and obtain results with increased accuracy

--- Support_Ticket_Analysis.py
+++ Support_Ticket_Analysis.py
@@ -12,6 +12,7 @@ from sklearn.cross_validation import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import confusion_matrix
 from nltk.stem.snowball import SnowballStemmer
+from sklearn.model_selection import GridSearchCV
 
 column_to_predict = pd.Series(["ticket_type","category","urgency","impact"])
 
@@ -28,6 +29,10 @@ min_data_per_class = 1
 y_pred = pd.DataFrame(columns = column_to_predict)
 prediction_acc = pd.DataFrame()
 pred = pd.DataFrame()
+best_accuracy = []
+best_parameters = []
+
+
 #dataframe = pd.read_csv('all_tickets.csv',dtype=str)
 dataframe = pd.read_csv('all_tickets.csv')
 text_columns = "body"
@@ -35,10 +40,10 @@ text_columns = "body"
 #tags = bytag[bytag.body >= min_data_per_class].index
 #dataframe = dataframe[dataframe[column_to_predict].isin(tags)]
 #Removing stop words and performing stemming using PIPELINE
-    count_vect = CountVectorizer(stop_words=stop_words_lang)
+count_vect = CountVectorizer(stop_words=stop_words_lang)
     #count_vect = StemmedCountVectorizer(stop_words=stop_words_lang)
     #Using Naive Bayes Classifier
-    text_clf = Pipeline([('vect', count_vect),('tfidf', TfidfTransformer()),('clf', MultinomialNB(fit_prior=fit_prior))])
+text_clf = Pipeline([('vect', count_vect),('tfidf', TfidfTransformer()),('clf', MultinomialNB(fit_prior=fit_prior))])
 
 for i in range(0,4):
     y = dataframe[column_to_predict[i]]
@@ -53,6 +58,24 @@ for i in range(0,4):
     prediction_acc = pd.concat([prediction_acc,pred],axis=1)
     
 prediction_acc.columns = column_to_predict
+
+parameters = [{'vect__ngram_range':[(1,1),(2,2)]},
+              {'tfidf__use_idf':[True,False]},
+              {'clf__alpha':[1e-2,1e-3]}]
+
+for i in range(0,4):
+    y = dataframe[column_to_predict[i]]
+    X = dataframe[text_columns]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20)
+    grid_search = GridSearchCV(estimator = text_clf,
+                               param_grid = parameters,
+                               n_jobs = -1
+                              )
+    grid_search = grid_search.fit(X_train,y_train)
+    best_accuracy.append(grid_search.best_score_)
+    best_parameters.append(grid_search.best_params_)
+    
+
 
 
 
@@ -71,7 +94,20 @@ for i in range(0,4):
     ticket_analysis = text_clf.predict(ticket_test)
     print(column_to_predict[i],"=",ticket_analysis)
     
-    
+for i in range(0,4):
+    y = dataframe[column_to_predict[i]]
+    X = dataframe[text_columns]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20)
+    #Using SVM Classifier
+    #text_clf = Pipeline([('vect', count_vect),('tfidf', TfidfTransformer()),('clf', SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3,n_iter=5, random_state=42))])
+    text_clf = text_clf.fit(X_train, y_train)
+    grid_search = GridSearchCV(estimator = text_clf,
+                               param_grid = parameters,
+                               n_jobs = -1
+                              )
+    grid_search = grid_search.fit(X_train,y_train)
+    ticket_analysis = grid_search.predict(ticket_test)
+    print(column_to_predict[i],"=",ticket_analysis)    
     
 #cm = confusion_matrix(y_test, y_pred)
 
